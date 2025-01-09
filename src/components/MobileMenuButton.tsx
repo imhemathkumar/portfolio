74efc2c6c62202b193990aa0f580f4df4d5a8758
Hemanth Kumar Telukuntla <hemanthkumartelukuntla143@gmail.com>
2025-01-10 02:16:36 +0530

    Update MobileMenuButton.tsx

--- src/components/MobileMenuButton.tsx
+++ src/components/MobileMenuButton.tsx
@@ -7,7 +7,7 @@ interface MobileMenuButtonProps {
 
 export function MobileMenuButton({ onClick }: Readonly<MobileMenuButtonProps>) {
   return (
-    <Button variant="ghost" size="icon" onClick={onClick} className="md:hidden">
+    <Button variant="ghost" size="icon" onClick={onClick} className="md:hidden dark:hover:bg-gray-700 dark:hover:text-white">
       <Menu className="h-5 w-5" />
       <span className="sr-only">Toggle menu</span>
       
