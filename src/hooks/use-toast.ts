60550dba5f23451a58c25484690e0c4af373ee26
Hemanth Kumar Telukuntla <hemanthkumartelukuntla143@gmail.com>
2025-01-09 22:59:50 +0530

    Update use-toast.ts

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -15,7 +15,7 @@ type ToasterToast = ToastProps & {
   action?: ToastActionElement;
 };
 
-const actionTypes = {
+const ACTION_TYPES = {
   ADD_TOAST: "ADD_TOAST",
   UPDATE_TOAST: "UPDATE_TOAST",
   DISMISS_TOAST: "DISMISS_TOAST",
@@ -29,7 +29,7 @@ function genId() {
   return count.toString();
 }
 
-type ActionType = keyof typeof actionTypes;
+type ActionType = keyof typeof ACTION_TYPES;
 
 type Action =
   | {
