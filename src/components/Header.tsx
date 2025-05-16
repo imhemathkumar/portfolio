4a1f9465dd94ed1aefadf8aca7755ffdf683e7e3
Hemanth Kumar Telukuntla <hemanthkumartelukuntla143@gmail.com>
2025-05-16 20:27:51 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,7 +22,7 @@ export default function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-inherit/80 dark:bg-gray-800/80 backdrop-blur-sm border-b dark:border-gray-700">
       <nav className="container mx-auto px-4 py-4 flex items-center justify-between space-x-4">
-        <Link href="/" className="text-xl hover:text-primary font-semibold dark:text-white"> <motion.div
+        <Link href="/" className="text-xl hover:text-primary dark:hover:text-primary font-semibold dark:text-white"> <motion.div
                       whileHover={{ rotate: 360 }}
                       transition={{ duration: 0.5 }}>
           Hemanth<span className="text-primary dark:text-primary hover:text-black dark:hover:text-white animate-pulse">.</span></motion.div>
